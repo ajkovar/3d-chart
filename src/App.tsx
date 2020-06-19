6aa1b54327649081612f2c9a18de1d24968bff38
Alex Kovar <ajkovar@gmail.com>
2020-06-19 11:39:20 -0500

    Initial not working attempt at animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
+import TWEEN from "@tweenjs/tween.js";
 import SceneManager from "./SceneManager";
-// import "./App.css";
+import "./App.css";
 
 function App() {
+  const [x, setX] = useState(50);
+  const [y, setY] = useState(30);
+  const [z, setZ] = useState(20);
+  let sceneManager: SceneManager | null = null;
+  const extractValue = (setter: Function) => (e: any) =>
+    setter(parseInt(e.target.value));
+
   useEffect(() => {
-    const sceneManager: SceneManager = new SceneManager(
+    sceneManager = new SceneManager(
       ref.current,
       window.innerWidth,
       window.innerHeight
     );
 
-    function animate() {
+    function animate(time: number) {
       requestAnimationFrame(animate);
-      sceneManager.update();
+      console.log("---------");
+      console.log("animate");
+      TWEEN.update(time);
+      sceneManager!.update();
     }
-    animate();
-  }, []);
+    animate(0);
+  });
+
+  useEffect(() => sceneManager!.animateVector({ x, y, z }), [
+    x,
+    y,
+    z,
+    sceneManager
+  ]);
 
   let ref: any = useRef();
-  return <div ref={ref} />;
+  return (
+    <div>
+      <div className="input-container">
+        <input value={x} type="number" onChange={extractValue(setX)} />
+        <input value={y} type="number" onChange={extractValue(setY)} />
+        <input value={z} type="number" onChange={extractValue(setZ)} />
+      </div>
+      <div ref={ref} />
+    </div>
+  );
 }
 
 export default App;
--- src/SceneManager.ts
+++ src/SceneManager.ts
@@ -4,24 +4,38 @@ import {
   Scene,
   LineBasicMaterial,
   BufferGeometry,
+  Geometry,
   Vector3,
   Line
 } from "three";
+import TWEEN from "@tweenjs/tween.js";
+import animateVector from "./utils/animateVector";
 import { OrbitControls } from "./OrbitControls";
+
+interface Coordinates {
+  x: number;
+  y: number;
+  z: number;
+}
 
 export default class SceneManager {
   scene: Scene;
   renderer: WebGLRenderer;
   camera: PerspectiveCamera;
   controls: OrbitControls;
+  line: Line;
+  lineGeometry: Geometry | undefined;
+  endpoint: Vector3 | undefined;
 
   constructor(container: HTMLElement, width: number, height: number) {
     this.scene = new Scene();
+    console.log(this.scene);
     this.renderer = this.initializeRenderer(width, height);
     this.camera = this.initializeCamera();
     this.controls = this.initializeControls();
     this.buildSubjects();
     container.appendChild(this.renderer.domElement);
+    this.line = new Line();
   }
   initializeCamera() {
     const camera: PerspectiveCamera = new PerspectiveCamera(
@@ -69,16 +83,67 @@ export default class SceneManager {
     ]);
     let axis: Line = new Line(axisGeometry, material);
 
-    let points: Vector3[] = [new Vector3(0, 0, 0), new Vector3(15, 20, 3)];
+    this.endpoint = new Vector3(15, 20, 3);
+    let points: Vector3[] = [new Vector3(0, 0, 0), this.endpoint];
 
-    let geometry: BufferGeometry = new BufferGeometry().setFromPoints(points);
-    let line: Line = new Line(geometry, material);
+    this.lineGeometry = new Geometry().setFromPoints(points);
+    this.line = new Line(this.lineGeometry, material);
+    this.line.name = "vector";
+    console.log(this.line);
 
     this.scene.add(axis);
-    this.scene.add(line);
+    this.scene.add(this.line);
+  }
+  findLine() {
+    return this.scene.children.find(child => child.name === "vector") as any;
+  }
+
+  animateVector({ x, y, z }: Coordinates) {
+    //remove previous tweens (if they exist)
+    TWEEN.removeAll();
+    console.log("animateVector");
+    const line = this.findLine();
+    new TWEEN.Tween(line.geometry.vertices[1])
+      .to({ x, y, z }, 2000)
+      .easing(TWEEN.Easing.Quadratic.In)
+      .onUpdate(d => {
+        const line = this.findLine();
+        line.geometry.vertices[1].x = Math.round(d.x);
+        line.geometry.vertices[1].y = Math.round(d.y);
+        line.geometry.vertices[1].z = Math.round(d.z);
+        console.log("tween", line.geometry.vertices[1]);
+        this.renderer.render(this.scene, this.camera);
+      })
+      .start();
+    // new TWEEN.Tween(this.endpoint)
+    //   .to({ x, y, z }, 2000)
+    //   .easing(TWEEN.Easing.Quadratic.In)
+    //   .onUpdate(d => {
+    //     console.log("update", d);
+    //     // console.log(this.lineGeometry);
+    //     // this.lineGeometry!.verticesNeedUpdate = true;
+    //     // this.lineGeometry!.elementsNeedUpdate = true;
+    //   })
+    //   .start();
   }
 
   update() {
+    // this.scene.remove(this.line);
+    // this.scene.children.forEach(child => {
+    //   if (child.name === "vector") {
+    //     // have to cast as any because am getting error about this prop
+    //     // not existing. why?
+    //     // https://github.com/mrdoob/three.js/blob/dev/src/core/Geometry.d.ts#L66
+    //     (child as any).geometry.vertices[1].x = 99;
+    //   }
+    // });
+    const line = this.findLine();
+    console.log("update", line.geometry.vertices[1]);
+    console.log(this.scene);
+
+    // line.geometry.vertices[1].x = -30;
+    // line.geometry.vertices[1].y = 20;
+    // line.geometry.vertices[1].z = -9;
     this.controls.update();
     this.renderer.render(this.scene, this.camera);
   }
